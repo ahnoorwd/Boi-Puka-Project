a0e22b06e7638f463e426d367d01fd92244f205f
ahmed nur <ahmednur103308@gmail.com>
2024-10-31 23:34:16 +0600

    Added all files for the project

---
+++ src/component/root/Root.jsx
@@ -0,0 +1,10 @@
+
+const Root = () => {
+    return (
+        <div>
+           <h1>i'am from root</h1>
+        </div>
+    );
+};
+
+export default Root;
--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,12 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Root from './component/root/Root.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element:<Root></Root>
   },
 ]);
 
